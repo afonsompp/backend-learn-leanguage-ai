3e47ffd8d79c6475621db1cd2721f331cf0c6b49
Afonso Mateus <afonsoprad10@gmail.com>
2024-06-15 17:03:18 -0400

    feat: add favicon

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,14 @@ import { BedrockModule } from './shared/aws/bedrock/bedrock.module';
 import { TextModule } from './geneate/text/text.module';
 import { AuthModule } from './shared/auth/auth.module';
 import { PromptModule } from './shared/prompt/prompt.module';
+import { join } from 'path';
+import { ServeStaticModule } from '@nestjs/serve-static';
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'), // path to the static files directory
+    }),
     ConfigModule.forRoot({
       ignoreEnvFile: true,
     }),
