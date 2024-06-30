020447e0a6f55c77852832f293bf835753e88a98
Afonso Mateus <afonsoprad10@gmail.com>
2024-06-30 12:02:32 -0400

    chore: config open api

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { setupSwagger } from './config/swegger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  setupSwagger(app);
   await app.listen(process.env.PORT || 8080);
 }
 
