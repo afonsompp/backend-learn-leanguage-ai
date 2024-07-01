12884c2c1870f4fc9e8d94b21358f25cf0af1ffd
Afonso Mateus <afonsoprad10@gmail.com>
2024-07-01 13:28:06 -0400

    chore: add auth to language resource

--- src/app/system/languages/controller/languages.controller.ts
+++ src/app/system/languages/controller/languages.controller.ts
@@ -8,32 +8,39 @@ import {
   ParseIntPipe,
   Post,
   Put,
+  UseGuards,
 } from '@nestjs/common';
 import { LanguagesService } from '@app/system/languages/service/languages.service';
 import { LanguageDto } from '@app/system/languages/dto/language.dto';
 import { CreateLanguageDto } from '@app/system/languages/dto/create-language.dto';
 import { UpdateLanguageDto } from '@app/system/languages/dto/update-language.dto';
+import { AuthGuard } from '@nestjs/passport';
+import { ScopesGuard } from '@core/security/scopes/scopes.guard';
 
 @Controller('languages')
 export class LanguagesController {
   constructor(private readonly languagesService: LanguagesService) {}
 
   @Get()
+  @UseGuards(AuthGuard('bearer'), ScopesGuard)
   findAll(): Promise<LanguageDto[]> {
     return this.languagesService.findAll();
   }
 
   @Get(':code')
+  @UseGuards(AuthGuard('bearer'), ScopesGuard)
   async findOne(@Param('code') code: string): Promise<LanguageDto> {
     return new LanguageDto(await this.languagesService.findOne(code));
   }
 
   @Post()
+  @UseGuards(AuthGuard('bearer'), ScopesGuard)
   create(@Body() language: CreateLanguageDto): Promise<LanguageDto> {
     return this.languagesService.create(language);
   }
 
   @Put(':id')
+  @UseGuards(AuthGuard('bearer'), ScopesGuard)
   update(
     @Param('id', new ParseIntPipe()) id: number,
     @Body() language: UpdateLanguageDto,
@@ -43,6 +50,7 @@ export class LanguagesController {
 
   @Delete(':id')
   @HttpCode(204)
+  @UseGuards(AuthGuard('bearer'), ScopesGuard)
   delete(@Param('id', new ParseIntPipe()) id: number): Promise<void> {
     return this.languagesService.delete(id);
   }
--- src/app/system/languages/languages.module.ts
+++ src/app/system/languages/languages.module.ts
@@ -3,9 +3,10 @@ import { LanguagesController } from './controller/languages.controller';
 import { LanguagesService } from './service/languages.service';
 import { Language } from './entities/language.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthorizationModule } from '@core/security/auth/authorization.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Language])],
+  imports: [TypeOrmModule.forFeature([Language]), AuthorizationModule],
   controllers: [LanguagesController],
   providers: [LanguagesService],
   exports: [LanguagesService],
