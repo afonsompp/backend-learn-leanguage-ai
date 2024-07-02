797b62d0c16c33f815a3ea65bf7cef226f429e82
Afonso Mateus <afonsoprad10@gmail.com>
2024-07-02 09:27:43 -0400

    fix: remove unecessary code

--- src/app/features/geneate/audio/dto/generate-audio.dto.ts
+++
@@ -1,8 +0,0 @@
-import { Expose } from 'class-transformer';
-import { IsString } from 'class-validator';
-
-export class GenerateAudioDto {
-  @Expose({ name: 'text' })
-  @IsString()
-  text: string;
-}
