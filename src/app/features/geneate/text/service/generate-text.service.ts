1f6b23943421d62c1c804b3324a826073489fbb1
Afonso Mateus <afonsoprad10@gmail.com>
2024-07-02 09:53:43 -0400

    chore: remove unnecessary code

--- src/app/features/geneate/text/service/generate-text.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { GenerateTextDto } from '@app/features/geneate/text/dto/generate-text.dto';
-
-@Injectable()
-export class GenerateTextService {
-  async create(request: GenerateTextDto): Promise<string> {
-    return request.textDescription;
-  }
-}
