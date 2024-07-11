3995a8c43f1c070a13badb700985bcfb6fe64771
Afonso Mateus <afonsoprad10@gmail.com>
2024-07-11 12:27:23 -0400

    refactor: simplify story module

--- src/app/features/story/story.module.ts
+++ src/app/features/story/story.module.ts
@@ -1,16 +1,13 @@
 import { Module } from '@nestjs/common';
-import { StoryTextModule } from '@app/features/story/text/story-text.module';
-import { RouterModule } from '@nestjs/core';
+import { AuthorizationModule } from '@core/security/auth/authorization.module';
+import { GenerateStoryTextController } from '@app/features/story/generator/controller/generate-story-text.controller';
+import { GenerateStoryTextService } from '@app/features/story/generator/service/generate-story-text.service';
+import { PracticeModule } from '@app/user/practice/practice.module';
+import { AIModule } from '@shared/ai/AI.module';
 
 @Module({
-  imports: [
-    StoryTextModule,
-    RouterModule.register([
-      {
-        path: 'story',
-        module: StoryTextModule,
-      },
-    ]),
-  ],
+  imports: [AuthorizationModule, PracticeModule, AIModule],
+  controllers: [GenerateStoryTextController],
+  providers: [GenerateStoryTextService],
 })
 export class StoryModule {}
